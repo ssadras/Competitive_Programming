3b1e987f02d3ee798b3edf08804365ad7b7ef551
ssadras <47696487+ssadras@users.noreply.github.com>
2022-06-21 12:15:23 +0430

    Added
    The solution to questions 2 part a, b, and c in exam 19 (second day of 3rd level of 24th computer olympiad) in Codeshark
    
    راه حل سوال بخش اول، دوم و سوم سوال 2 آزمون 19 (مرحله 3 روز اول دوره 24 ام المپیاد کامپیوتر ایران) کدشارک

---
+++ Codeshark/Exam19/2-A,B,C.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Pragmas here
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx2,tune=native")
+
+// Templates here
+template<typename T1, typename T2, typename T3>
+class triple {
+public:
+    T1 first;
+    T2 second;
+    T3 third;
+
+    triple() {
+        first = 0;
+        second = 0;
+        third = 0;
+    }
+};
+
+// Typedefs here
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef set<pii> spii;
+typedef set<pll> spll;
+typedef map<string, int> mpsi;
+typedef map<string, ll> mpsl;
+typedef map<int, int> mpii;
+typedef map<ll, ll> mpll;
+
+// Defines here
+#define IOS ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define fora(i, a, b) for(int (i)=(a);(i)<(b);(i)++)
+#define forla(i, a, b) for(ll (i)=(a);(i)<(b);(i)++)
+#define for0(i, b) fora((i),0,(b))
+#define forl0(i, b) forla((i),0,(b))
+#define rofa(i, a, b) for(int (i)=(a);(i)>=(b);(i)--)
+#define rofla(i, a, b) for(ll (i)=(a);(i)>=(b);(i)--)
+#define rof0(i, a) rofa((i),(a),0)
+#define rofl0(i, a) rofla((i),(a),0)
+#define forag(i, a, b, g) for(int (i)=(a);(i)<(b);(i)+=(g))
+#define forlag(i, a, b, g) for(ll (i)=(a);(i)<(b);(i)+=(g))
+#define for0g(i, b, g) forag((i),0,(b),(g))
+#define forl0g(i, b, g) forlag((i),0,(b),(g))
+#define rofag(i, a, b, g) for(int (i)=(a);(i)>=(b);(i)-=(g))
+#define roflag(i, a, b, g) for(ll (i)=(a);(i)>=(b);(i)-=(g))
+#define rof0g(i, a, g) rofag((i),(a),0,(g))
+#define rofl0g(i, a, g) roflag((i),(a),0,(g))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define pf push_front
+#define MP make_pair
+#define F first
+#define S second
+#define GraphM1(g, n) int (g)[(n)][(n)]
+#define GraphM2(g, n) vi (g)[(n)]
+
+
+// Const variables here
+const int delta = 10847;
+const int maxn = 3000;
+
+// Before structs global variables and functions here
+
+
+// Structs here
+
+
+// Global variables here
+int fact[maxn];
+vi a[maxn];
+int dp[maxn];
+
+// Functions here
+inline int add(int x, int y) {
+    int c = x + y;
+
+    if (c >= delta)
+        c -= delta;
+
+    if (c < 0)
+        c += delta;
+
+    return c;
+}
+
+inline int mul(int x, int y) {
+    return 1ll * x * y % delta;
+}
+
+inline int pwr(int x, int y) {
+    int ans = 1;
+
+    while (y) {
+        if (y & 1)
+            ans = mul(ans, x);
+
+        y /= 2;
+        x = mul(x, x);
+    }
+
+    return ans;
+}
+
+int inv(int n) {
+    return pwr(n, delta - 2);
+}
+
+int C(int n, int k) {
+    return mul(mul(fact[n], inv(fact[k])), inv(fact[n - k])) % delta;
+}
+
+// Main here
+int main() {
+
+    a[1] = {1};
+    a[2] = {1, 1};
+    fora(i, 3, maxn) {
+        a[i].pb(1);
+
+        for0(j, a[i - 1].size() - 1)a[i].pb(add(a[i - 1][j], a[i - 1][j + 1]));
+
+        a[i].pb(1);
+    }
+
+    int n;
+    cin >> n;
+
+    dp[2] = 0;
+    dp[3] = 1;
+
+    fora(i, 4, 2020) {
+        dp[i] = mul(2, dp[i - 1]);
+
+        int tmp = 0, tmp2 = 0;
+        for0(j, a[i].size() - 2) {
+            tmp = add(tmp, a[i][j]);
+
+
+
+            tmp2 = add(mul(tmp, a[i][j + 2]), tmp2);
+        }
+
+        dp[i] = add(dp[i], tmp2);
+    }
+
+    cout << dp[n] << ' ' << pwr(dp[n], n) << "\n";
+
+
+}
